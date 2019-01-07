817b46cd1c722f22f9d03e17c4e9317de854267a
Mamadou Samba <mamadou02@outlook.com>
2019-01-07 23:03:15 +0000

    Almost Complete

--- js/script.js
+++ js/script.js
@@ -1,5 +1,39 @@
 /* global $ */
+function q1Score(q1Result) {
+    if(q1Result==="Yes"){
+        return 1;
+    }else if(q1Result==="No"){
+        return 2;
+    }else {
+     return 3;
+}
 
+}
+function q2Score(q2Result){
+    if(q2Result==="Humanity"){
+        return 2;
+    }else {
+        return 3;
+    }
+    
+}
+function q3Score(q3Result){
+    if(q3Result==="Religious to the end"){
+        return 3;
+    }else {
+        return 4;
+   
+}
+function endShow(name, techPlacement){
+    return "Congrats" + " " + name + "! Your Castlevania character is" + " " + techPlacement + "." ;
+}
+function characterName(totalScore){
+    if(totalScore<4){
+        return "Gabriel";
+    } else{
+        return "Alucard";
+    }
+}
 $(document).ready(function() {
     $("button").click(function() {
         var name = $("#name").val();
@@ -8,15 +42,20 @@ $(document).ready(function() {
         var techPlacement = "TBD";
         var imgSrc = "https://media.giphy.com/media/22zgHX8aop488/giphy.gif";
         var q1Result = $("#question1").val();
+        
         var q2Result = $("#question2").val();
         var q3Result = $("#question3").val();
-        var totalScore;
+        var totalScore=0;
         
-        $(".result").html(q1Result);
+        
+        totalScore=totalScore+q1Score(q1Result);
+        totalScore=totalScore+q2Score(q2Result);
+        totalScore=totalScore+q3Score(q3Result);
+        techPlacement=characterName(totalScore);
+        var yourCharacter=endShow(name, techPlacement);
+        
+        $(".result").html(yourCharacter);
         
     });
 
 });
-function enjoyment(name){
-    return 1;
-}
